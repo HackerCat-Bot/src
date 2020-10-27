77c189f2d6cdfe61c2bd45a3807bbce8477a9adc
ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>
2020-10-27 21:25:56 +0100

    Uploading QR code source

---
+++ qr.js
@@ -0,0 +1,10 @@
+/**
+* @author ParliamoDiPC
+* @description QR command's source code.
+*/
+
+const args = message.content.slice(5)
+
+message.channel.send(`<:qr:770744564666466364>: https://chart.apis.google.com/chart?cht=qr&chs=100x100&choe=UTF-8&chld=H|0&chl=${args}`)
+
+/* HackerCat's code. https://github.com/HackerCat-Bot/src */
